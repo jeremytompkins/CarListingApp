323473197d79393b3c3ac04a9c5a0f1e6172756f
jeremytompkins <jeremy@thetompkinsfamily.com>
2022-08-06 20:44:39 -0500

    Initial Commit

---
+++ Car Listing App Challenge/Car_Listing_App_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Car_Listing_App_ChallengeApp.swift
+//  Car Listing App Challenge
+//
+//  Created by Jeremy Tompkins on 8/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Car_Listing_App_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
